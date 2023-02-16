ad543178aa0f2f622a37079c38196773fd520c85
Pang <pbw1998@126.com>
2023-02-16 11:02:18 +0800

    fix sql

--- datebase/e-hospital.sql
+++ datebase/e-hospital.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80032
  File Encoding         : 65001
 
- Date: 13/02/2023 00:24:47
+ Date: 16/02/2023 11:02:02
 */
 
 SET NAMES utf8mb4;
@@ -102,7 +102,6 @@ DROP TABLE IF EXISTS `doctors_registration`;
 CREATE TABLE `doctors_registration`  (
   `id` int(0) NOT NULL AUTO_INCREMENT,
   `Fname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `Mname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `Lname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `Age` int(0) NOT NULL,
   `bloodGroup` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
@@ -123,14 +122,16 @@ CREATE TABLE `doctors_registration`  (
   `uuid` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `verification` tinyint(1) NULL DEFAULT NULL,
+  `FullName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of doctors_registration
 -- ----------------------------
-INSERT INTO `doctors_registration` VALUES (1, 'Demo', '', 'Test', 18, 'O', '123456789', 'test@test.com', 'test@test.com', 'Ottawa', '', 'Test', 'Ottawa', 'ON', 'Canada', '123456789-ABC', '123456789-ABC', '123456789-ABC', '123456789-ABC', 'Male', 'DOC123', 'DOC123', 1);
-INSERT INTO `doctors_registration` VALUES (5, 'a', 'a', 'a', 0, 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'DOC-ON-a-a-6033310000', 'a8d3bdc110bf60c3dc114e27f1706a3c', 0);
+INSERT INTO `doctors_registration` VALUES (1, 'Demo', 'Test', 18, 'O', '123456789', 'test@test.com', 'test@test.com', 'Ottawa', '', 'Test', 'Ottawa', 'ON', 'Canada', '123456789-ABC', '123456789-ABC', '123456789-ABC', '123456789-ABC', 'Male', 'DOC123', 'DOC123', 1, NULL);
+INSERT INTO `doctors_registration` VALUES (5, 'a', 'a', 0, 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'DOC-ON-a-a-6033310000', 'a8d3bdc110bf60c3dc114e27f1706a3c', 0, NULL);
+INSERT INTO `doctors_registration` VALUES (6, 'Doctor', 'Test', 12, 'A', '0132322323232', 'test1234@test.com', 'test123@test.com', '123', '123', '1111', 'SA', '123', '中国', '123', '123', '123', '123', 'male', 'DOC-ON-12-123-818510000', '123', 1, 'Doctor Test');
 
 -- ----------------------------
 -- Table structure for hospital_admin
@@ -221,7 +222,6 @@ DROP TABLE IF EXISTS `patients_registration`;
 CREATE TABLE `patients_registration`  (
   `id` int(0) NOT NULL AUTO_INCREMENT,
   `FName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `MName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `LName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `Age` int(0) NOT NULL,
   `BloodGroup` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
@@ -241,30 +241,16 @@ CREATE TABLE `patients_registration`  (
   `password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `verification` tinyint(1) NULL DEFAULT NULL,
   `isSick` int(0) NULL DEFAULT NULL COMMENT 'type 1: sicked, type 2: unsicked',
+  `FullName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `CaseName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 24 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 28 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of patients_registration
 -- ----------------------------
-INSERT INTO `patients_registration` VALUES (1, 'Test', '', 'Demo', 18, 'O', '123456789', 'test@test.com', 'Testing Address', 'Ottawa', 'Test123', 'Ottawa', 'Ontario', '123456789-ABC', '123456789-ABC', '123456789-ABC', '123456789-ABC', 'Male', 'PAT123', 'PAT123', 1, 1);
-INSERT INTO `patients_registration` VALUES (7, 'demo', '12', 'demo', 12, 'O', '123456789', 'demo@admin.com', 'University', 'Ottawa', 'Ottawa', 'Ottawa', '122324465', '12324325', '1263521365123', '235613213615', '21365123621536', 'Male', 'PAT-ON-12-122324465-5622310000', 'a9b2f4e0b8d81cdc0d276b9e757417b6', 1, 0);
-INSERT INTO `patients_registration` VALUES (8, 'Yolo', 'Polo', 'Golo', 35, 'O', '2436576879', 'TRETFTHT@gmail.com', '1234', 'Ottawa', 'k1bfhyt', 'dgry', 'wfey5', 'grty6tughjyk', 'ngfjyuiytuky', 'frhgjgykumvm', 'nhyjtyitu76ti', 'Male', 'PAT-ON-35-wfey5-7648010000', 'e143e66717960f8f24978ea7d20bcf37', 0, 0);
-INSERT INTO `patients_registration` VALUES (9, 'Dongchen', ' ', 'Gu', 24, 'B', '', 'dgu068@uottawa.ca', '320 Maclaren street', 'ottawa', 'K2P 0M6', 'ottawa', 'ontario', 'n/a', 'n/a', 'n/a', 'n/a', 'male', 'PAT-ON-24-ontario-3943910000', '2b79fe2866b6b6756cf960039c2de9ec', 1, 1);
-INSERT INTO `patients_registration` VALUES (10, 'jk', 'null', 'test', 18, 'b', '16131234567', 'jliu475@uottawa.ca', '171 Lees Avenue', 'Ottawa', 'K1S 5P3', 'Ottawa', 'ON', 'test', 'test', 'test', 'test', 'Male', 'PAT-ON-18-ON-5191710000', '15799df3881cc57cef2975b470f8631f', 0, 0);
-INSERT INTO `patients_registration` VALUES (11, 'Sick', '-', 'Guy', 98, 'C+', '9876543210', 'sickguy@admin.com', '123 xyz', 'abc', 'a2f c2z', 'asd', 'asd', '123245456', '123345436356', '23325346536', '2534635657', 'Male', 'PAT-ON-98-asd-1941310000', '393655fd6c84d02dd18254f33d76ca4f', 1, 0);
-INSERT INTO `patients_registration` VALUES (12, 'abc', 'abc', 'abc', 23, 'O', '1234567890', 'mkagz064@uottawa.ca', 'asafaf', 'asfasfasf', 'asfafaf', 'Ottawa', 'ON', '425436253', '243235346', '325246357', '235346547', 'Male', 'PAT-ON-23-ON-4910510000', '0ccf472a01b3517a4729f73547db64c4', 1, 0);
-INSERT INTO `patients_registration` VALUES (13, 'Hoseintest55', 'Hosein', 'Sadat Hosseini', 35, 'o+', '43543534543', 'HospitalTest2@gmail.com', '45435435', '33333', '4353453', 'Ottawa', 'ON', '34343434', '324234324', '324242424', '32432424', 'male', 'PAT-ON-35-ON-8572910000', 'TestH', 1, 1);
-INSERT INTO `patients_registration` VALUES (14, 'test', 'test', 'test', 1, 'A', '111-222-3333', 'test@test.ca', 'a', 'ottawa', 'A1A2BC', 'Ottawa', 'ON', '1112223333', '111-1111-1111', '112', 'A1A2BC', 'male', 'PAT-ON-1-ON-663510000', 'c05f008d81ec86e11c8935d2e703092c', 0, 0);
-INSERT INTO `patients_registration` VALUES (15, 'nnnn', 'Hosein', 'Sadat Hosseini', 35, 'd', '5646465654', 'HospitalTest2@gmail.com', '45435435', 'u', '4353453', 'Ottawa', 'ON', 'o', 'i', 'u', 'h', 'dwefmd', 'PAT-ON-35-ON-1730710000', 'b150a358e4def1c06e32c6d9bbe001b4', 1, 0);
-INSERT INTO `patients_registration` VALUES (16, 'test', 'tess', 'tes', 12, 'a', '132322323232', '123', '123', '123', '13', '123', '123', '123', '123', '123', '123', 'ts', 'PAT-ON-12-123-2375410000', '2c29b857913d4abeb8114ac4f37b9aad', 0, 0);
-INSERT INTO `patients_registration` VALUES (17, 'mndfownflw', 'jkjbk ', 'kjfhuhf', 65, 'a', '123456789', 'kjbdkjw @gmail.com', ',wnfowjfl f', 'khbgoehbdl,', 'khflwed', 'jvfiughljnm ', 'jhgiubj', '54984132798', 'efer5456', '35rfr21946', '6565323e', 'Male', 'PAT-ON-65-jhgiubj-5847910000', 'a43792493773c7146a45a49ad8b823ef', 0, 1);
-INSERT INTO `patients_registration` VALUES (18, 'a', 'a', 'a', 0, 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'aa', 'a', 'a', 'a', 'PAT-ON-a-a-602110000', 'c5867a228b0f3eb1ca787f399f30d8dd', 0, 0);
-INSERT INTO `patients_registration` VALUES (19, 'SMS TEst', 'Hosein', 'Sadat Hosseini', 35, 'd', '16132622138', 'HospitalTest2@gmail.com', '45435435', 'u', '4353453', 'Ottawa', 'ON', 'o', 'i', 'u', 'h', 'dwefmd', 'PAT-ON-35-ON-6204510000', 'e4f363febca19626fd34402efc9b4eb8', 0, 0);
-INSERT INTO `patients_registration` VALUES (20, 'SMS TEst', 'Hosein', 'Sadat Hosseini', 35, 'd', '16132622138', 'HospitalTest2@gmail.com', '45435435', 'u', '4353453', 'Ottawa', 'ON', 'o', 'i', 'u', 'h', 'male', 'PAT-ON-35-ON-4544810000', '4facc5ee5e77168d2659af3ec17a4b92', 0, 1);
-INSERT INTO `patients_registration` VALUES (21, 'SMS TEst', 'Hosein', 'Sadat Hosseini', 234213, '231313', '16132622138', 'HospitalTest2@gmail.com', '45435435', '213133', '4353453', 'Ottawa', 'ON', '213131', '321321313', '3213131', '321313', 'dwefmd', 'PAT-ON-234213-ON-6204510000', '6cba6c12b9905fd41400402f5c55ee09', 0, 1);
-INSERT INTO `patients_registration` VALUES (22, 'SMS TEst', 'Hosein', 'Sadat Hosseini', 0, 'd', '16132622138', 'HospitalTest2@gmail.com', '45435435', 'u', '4353453', 'Ottawa', 'ON', 'o', 'i', 'u', '32432424', 'dwefmd', 'PAT-ON-sdfds-ON-2480810000', '2fd1b7f314a8726e83ea3956ef4f9121', 0, 0);
-INSERT INTO `patients_registration` VALUES (23, 'TestH fdsfd', 'Hosein', 'Sadat Hosseini', 35, 'd', '16132622138', 'HospitalTest2@gmail.com', '45435435', 'u', '4353453', 'Ottawa', 'ON', 'o', 'i', 'u', '32432424', 'dwefmd', 'PAT-ON-35-ON-416210000', '28497cc0f32d970b8ab999786de43ee6', 0, 0);
+INSERT INTO `patients_registration` VALUES (1, 'Test', 'Demo', 18, 'O', '123456789', 'test@test.com', 'Testing Address', 'Ottawa', 'Test123', 'Ottawa', 'Ontario', '123456789-ABC', '123456789-ABC', '123456789-ABC', '123456789-ABC', 'Male', 'PAT123', 'PAT123', 1, 1, NULL, NULL);
+INSERT INTO `patients_registration` VALUES (31, 'F', 'L', 24, 'A', '0132322323232', 'test1236@test.com', 'Address', 'SA', '1111', 'SA', '123', '13', '123', '123', '123', 'male', 'PAT-ON-24-123-265010000', '123', 1, 0, 'F L', 'Normal: [(222, 505),(504, 770),(770, 1044),(1044, 1339),(1339, 1631),(1631, 1926),(1926, 2205),(2205, 2492),(2491, 2794),(2793, 3067),(3066, 3357)];');
 
 -- ----------------------------
 -- Table structure for sickness_record
@@ -274,22 +260,21 @@ CREATE TABLE `sickness_record`  (
   `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
   `create_time` datetime(0) NULL DEFAULT NULL COMMENT 'Create Time',
   `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `patient_id` int(0) NULL DEFAULT NULL COMMENT 'patient\'s id',
   `isSick` tinyint(1) NULL DEFAULT NULL,
   `diagnosis` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT 'doctor write diagnosis according uploaded images',
   `doctor_id` int(0) NULL DEFAULT NULL COMMENT 'doctor id',
   `PatientName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `DoctorName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `Gender` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `uuid` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `CaseName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 55 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sickness_record
 -- ----------------------------
-INSERT INTO `sickness_record` VALUES (2, NULL, NULL, NULL, 1, NULL, NULL, '1', NULL, '2');
-INSERT INTO `sickness_record` VALUES (3, NULL, NULL, NULL, 1, NULL, NULL, 'Leo', NULL, 'man');
-INSERT INTO `sickness_record` VALUES (4, NULL, NULL, NULL, 1, NULL, NULL, 'Dongchen', NULL, 'man');
+INSERT INTO `sickness_record` VALUES (72, NULL, NULL, 0, NULL, NULL, 'F L', NULL, 'male', 'PAT-ON-24-123-265010000', 'Normal: [(222, 505),(504, 770),(770, 1044),(1044, 1339),(1339, 1631),(1631, 1926),(1926, 2205),(2205, 2492),(2491, 2794),(2793, 3067),(3066, 3357)];');
 
 -- ----------------------------
 -- Table structure for tmp_etl_table
